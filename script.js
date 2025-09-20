136f92144ab9d421e55f998a2bf48b5da0cdb146
ArtemiiHH <artemiishchukin2003@gmail.com>
2025-09-20 13:53:38 +0300

    Add Player, gameController Constructors

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function Gameboard() {
+    const gameBoard = [];
+    const columns = 3;
+    const rows = 3;
+};
 
+function Player(name, symbol) {
+    this.name = name;
+    this.symbol = symbol;
+};
+
+function gameController() {
+    
 };
