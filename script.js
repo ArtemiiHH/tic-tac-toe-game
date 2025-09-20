f3848eb68e7501badef4804926a0b2946b0ae04f
ArtemiiHH <artemiishchukin2003@gmail.com>
2025-09-20 15:11:36 +0300

    Change val to cellId for better understanding

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const gameBoard = (function Gameboard() {
     const columns = 3;
     const rows = 3;
     const board = [];
-    let cell = 0;
+    let cellId = 0;
 
     for (let i = 0; i < rows; i++) {
         board[i] = [];
 
         for (let j = 0; j < columns; j++) {
-            board[i][j] = cell++;
+            board[i][j] = cellId++;
         }
     }
 
@@ -17,9 +17,15 @@ const gameBoard = (function Gameboard() {
     // const getBoard = () => board;
 })();   
 
+
+
+
 function Player(name, symbol) {
     return { name, symbol };
 };
+
+
+
 
 const gameController = (function GameController() {
 
