f566adc504e6c73789483869ae04548f2be6a9ec
ArtemiiHH <artemiishchukin2003@gmail.com>
2025-09-20 15:05:06 +0300

    Create a 2d array in gameBoard

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const gameBoard = (function Gameboard() {
     const columns = 3;
     const rows = 3;
     const board = [];
-})();
+    let cell = 0;
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+
+        for (let j = 0; j < columns; j++) {
+            board[i][j] = cell++;
+        }
+    }
+
+    console.log(board);
+
+    // const getBoard = () => board;
+})();   
 
 function Player(name, symbol) {
     return { name, symbol };
